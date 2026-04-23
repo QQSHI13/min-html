6db336a54baab4945e261f83c2ae44c44be5f6e0
AshfordN <ashford.nichols@gmail.com>
2026-04-23 01:21:32 -0400

    Update element.rs
    removed incorrect/misleading comment

--- minify-html/src/parse/element.rs
+++ minify-html/src/parse/element.rs
@@ -104,7 +104,6 @@ pub fn parse_tag(code: &mut Code) -> ParsedTag {
         loop {
           attr_name.extend_from_slice(code.slice_and_shift_while_not_seq_case_insensitive(closing));
 
-          // the template syntax closing word should be followed by a whitespace or the end of the tag (i.e. '>' or '/>')
           match code.shift_if_next_not_in_lookup(WHITESPACE_OR_SLASH_OR_EQUALS_OR_RIGHT_CHEVRON) {
             Some(c) => attr_name.push(c),
             None => break,
